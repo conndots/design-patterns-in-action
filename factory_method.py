cf427b43a9d40a67b032f90f9a569abdb5661e81
skyline0623 <skyline0623@gmail.com>
2015-03-31 20:09:00 +0800

    update descriptions of factory_method.py

--- factory_method.py
+++ factory_method.py
@@ -3,6 +3,7 @@
 
 """
 Factory method is a design pattern. It is spposed to design an interface for creating an object, but let subclasses decide which class to instantiate. It lets a class defer instantiation to subclasses.
+In this file displays a typical usage of factory method: connecting parallel class hierarchies. Hierarchies of Figure gives implementation of figures and hierarchies of manipulators give corresponding tools to manipulate specific figures.We use factory method patterns to create manipulators.
 """
 class FigureManipulator(object):
 	def __init__(self, figure):
@@ -35,14 +36,17 @@ class TextManipulator(FigureManipulator):
 		print("Text manipulate: move figure left to {} pixels and up to {} pixels.".format(target['left'], target['top']))
 
 class Figure(object):
+	"Basic class of a figure which creates a default manipulator."
 	def __init__(self, name, url):
 		self._name = name
 		self._url = url
 
+	"factory method"
 	def get_manipulator(self):
 		return FigureManipulator(self)
 
 class LineFigure(Figure):
+	"A subclass to specify one kind of specific kind of figures, which creates a specific manipulator"
 	def __init__(self, name, url, line_pos):
 		super(LineFigure, self).__init__(name, url)
 		self._line_pos = line_pos
@@ -51,6 +55,7 @@ class LineFigure(Figure):
 		return LineManipulator(self)
 
 class TextFigure(Figure):
+	"A subclass to specify one kind of specific kind of figures, which creates a specific manipulator"
 	def __init__(self, name, url, text):
 		super(TextFigure, self).__init__(name, url)
 		self._text = text
@@ -67,5 +72,22 @@ if __name__ == '__main__':
 		manipulator.drag({"left": 10, "top": -15})
 		print("=" * 10)
 
-
+"""
+OUTPUT:
+For figure: a
+Default manipulate: move up figure to: 10 pixels.
+Default manipulate: move down figure to: 5 pixels.
+Default manipulate: move figure left to 10 pixels and up to -15 pixels.
+==========
+For figure: b
+Line manipulate: move up figure to: 10 pixels.
+Line manipulate: move down figure to: 5 pixels.
+Line manipulate: move figure left to 10 pixels and up to -15 pixels.
+==========
+For figure: c
+Text manipulate: move up figure to: 10 pixels.
+Text manipulate: move down figure to: 5 pixels.
+Text manipulate: move figure left to 10 pixels and up to -15 pixels.
+==========
+"""
 
